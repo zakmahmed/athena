58aae33d91a842fe41c3229eab52e99947c774fd
Zakariya Ahmed Mohamed <zak2mahmed@gmail.com>
2023-05-27 23:04:15 +0100

    Design basic window
    - Reduce window size to 200 x 300
    - Set background color to white
    - Add icon image to directory

--- athena_gui.py
+++ athena_gui.py
@@ -1,9 +1,32 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
-from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtGui import QIcon, QPixmap, QColor
+from PyQt6.QtCore import QSize, Qt
 
+class AthenaGUI(QMainWindow):
+    def __init__(self):
+        super(AthenaGUI, self).__init__()
+        self.GuiInit()
+
+    def GuiInit(self):
+        self.setWindowTitle('Athena')
+        self.setMinimumSize(QSize(200,300))
+
+        self.label = QLabel()
+        self.pixmap = QPixmap('icon.png')
+        self.label.setPixmap(self.pixmap)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setCentralWidget(self.label)
+
+        self.myPalette = self.palette()
+        self.myPalette.setColor(self.backgroundRole(), QColor(255,255,255))
+        self.setPalette(self.myPalette)
+
+
+        self.show()
+        
+
+    
 app = QApplication(sys.argv)
-
-window = QWidget()
-window.show()
-app.exec()
+window = AthenaGUI()
+sys.exit(app.exec())
