f695c04b1e0a7a21cd2207d2b9b99d3338dc3e29
Zakariya Ahmed Mohamed <zak2mahmed@gmail.com>
2023-07-02 22:10:53 +0100

    Remove informal greeting

--- athena_logic.py
+++ athena_logic.py
@@ -1,4 +1,4 @@
-import speech_recognition as sr 
+import speech_recognition as sr
 from datetime import date
 import time
 import os
@@ -6,13 +6,13 @@ from gtts import gTTS
 import playsound
 import random
 import sys
-import pyjokes 
+import pyjokes
 
 MASTER = 'Zakariya'
 
+
 class Athena:
     def __init__(self):
-        self.greeting()
         self.run()
 
     def greeting(self):
@@ -21,27 +21,25 @@ class Athena:
         match self.current_hour:
             case _ if self.current_hour > 0 and self.current_hour < 12:
                 self.speak(f'Good Morning, {MASTER}')
-            case _ if self.current_hour >= 12  and self.current_hour < 17:
+            case _ if self.current_hour >= 12 and self.current_hour < 17:
                 self.speak(f'Good Afternoon, {MASTER}')
             case _ if self.current_hour >= 17:
                 self.speak(f'Good Evening, {MASTER}')
 
-
     def run(self):
-        self.speak("Hey Zak what can I do for you?")
+        self.greeting()
         while 1:
             self.data = self.recordAudio()
             self.make_decision(self.data)
-            
-    def speak(self, audioString):         
-        tts = gTTS(text = audioString, lang = 'en-gb', tld='co.uk')
-        r1 = random.randint(1,1000)
+
+    def speak(self, audioString):
+        tts = gTTS(text=audioString, lang='en-gb', tld='co.uk')
+        r1 = random.randint(1, 1000)
         self.audiofile = str(r1)+"audio.mp3"
         tts.save(self.audiofile)
         print(audioString)
         playsound.playsound(self.audiofile)
         os.remove(self.audiofile)
-        
 
     def recordAudio(self):
         r = sr.Recognizer()
@@ -53,11 +51,12 @@ class Athena:
             try:
                 self.data = r.recognize_google(audio)
             except Exception:
-                print("Unfortunately, I could not understand what you just said, please try again")
-        
+                print(
+                    "Unfortunately, I could not understand what you just said, please try again")
+
             return self.data
 
-    def make_decision(self,data):
+    def make_decision(self, data):
         print(data)
         if "how are you" in data:
             self.speak("I am fine")
@@ -69,21 +68,17 @@ class Athena:
 
         if "time" in data:
             t = time.localtime()
-            self.current_time = time.strftime("%I:%M %p",t)
+            self.current_time = time.strftime("%I:%M %p", t)
             self.speak(self.current_time)
 
         if "joke" in data:
             self.speak(pyjokes.get_joke(category='neutral'))
 
+    # Websearch
 
-
-    #Websearch
-               
-    
-    #Calendar API
+    # Calendar API
 
         if "that is all" in self.data:
             self.speak("Very Well")
             # os.remove(self.audiofile)
             sys.exit(0)
-        
